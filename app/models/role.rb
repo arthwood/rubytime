23a92a7bbd9c06ff417f7bc57fadc0dfa613ba7b
unknown <qwerty@.(none)>
2010-04-02 00:29:34 +0200

    Roles; Users.js

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,7 +1,5 @@
 class Role < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
-  validates_presence_of :can_manage_financial_data
-  
-  ROLES = ['developer', 'project manager', 'tester']
+  validates_inclusion_of :can_manage_financial_data, :in => [true, false]
 end
--- public/javascripts/Users.js
+++ public/javascripts/Users.js
@@ -1,7 +1,28 @@
 var Users = function() {
+  this.userType = $('user_type');
+  this.userRole = $('user_role_id');
+  /*this.userRoleItem = this.userRole.up('.item').first();
+  this.client = $('user_client_id');
+  this.clientItem = this.client.up('.item').first();
+  
+  this.selects = [this.userRoleItem, this.clientItem];
+
+  this.userType.onchange = $DC(this, this.onUserTypeChange);
+
+  this.updateSelect();*/
 }
 
 Users.prototype = {
+  onUserTypeChange: function(e) {
+    this.updateSelect();
+  },
+
+  updateSelect: function() {
+    var value = parseInt(this.userType.value)
+    
+    this.selects[value].show();
+    this.selects.reverse()[value].hide();
+  }
 };
 
 Application.onLoad.add($D(null, function() {
