0b4129924dff1b73a37263b3a017876ba7497ca8
artur bilski <arthwood@llp.pl>
2010-04-29 11:25:31 +0200

    filterinf activities by invoice

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -7,6 +7,8 @@ class Activity < ActiveRecord::Base
   validate :time_spent_values
   
   named_scope :for_day, lambda {|date| {:conditions => {:date => date}}}
+  named_scope :invoiced, :conditions => 'invoice_id IS NOT NULL'
+  named_scope :not_invoiced, :conditions => 'invoice_id IS NULL'
   
   def as_json(options = {})
     super(:include => [:project, :user], :methods => :time_spent)
@@ -20,15 +22,17 @@ class Activity < ActiveRecord::Base
     @time_spent = v
   end
   
-  # filter {:project_id => 2, :date => {:from => '06-04-2010', :to => '21-04-2010'}, :include => 'all', :user_id => 3}}
+  # filter {:project_id => 2, :date => {:from => '06-04-2010', :to => '21-04-2010'}, :invoice_filter => 'all', :user_id => 3}}
   def self.search(filter)
     project_id = filter.project_id
     user_id = filter.user_id
     client_id = filter.client_id
+    invoice_filter = filter.invoice_filter
     conditions = {}
     conditions[:project_id] = project_id unless project_id.blank?
     conditions[:user_id] = user_id unless user_id.blank?
     conditions['projects.client_id'] = client_id unless client_id.blank?
+    scope = (invoice_filter.blank? && :all) || invoice_filter.to_sym
     from, to = filter.from, filter.to
     from = from.blank? ? nil : Date.parse(from)
     to = to.blank? ? nil : Date.parse(to)
@@ -38,7 +42,8 @@ class Activity < ActiveRecord::Base
       LEFT OUTER JOIN projects ON (projects.id = project_id)
       LEFT OUTER JOIN clients ON (clients.id = projects.client_id)
     }
-    all(:conditions => conditions, :joins => joins, :order => 'date DESC')
+    
+    send(scope, :conditions => conditions, :joins => joins, :order => 'date DESC')
   end
   
   protected
