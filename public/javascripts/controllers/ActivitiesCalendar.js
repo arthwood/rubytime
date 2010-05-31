f4d6c48ce26ea865eef3d8e5392af22551679fa5
artur bilski <arthwood@llp.pl>
2010-05-31 17:35:20 +0200

    code update due to ArtJs changes

--- public/javascripts/controllers/ActivitiesCalendar.js
+++ public/javascripts/controllers/ActivitiesCalendar.js
@@ -65,7 +65,7 @@ ActivitiesCalendar.prototype = {
     var element = mc.element;
     
     if (element.hasClass('active')) {
-      var related = mc.targets(e, false).related;
+      var related = mc.getTargets(e, false).related;
       
       if (!(related == this.details || related.descendantOf(this.details))) {
         this.hideDetails();
@@ -80,7 +80,7 @@ ActivitiesCalendar.prototype = {
   },
   
   onDetailsOut: function(e, mc) {
-    if (mc.targets(e, false).related != this.detailsCell) {
+    if (mc.getTargets(e, false).related != this.detailsCell) {
       this.hideDetails();
     }
       
