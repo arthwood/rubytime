f0a2e68714315ca7bad3af36431172bff9c7251b
artur bilski <arthwood@llp.pl>
2010-06-23 12:28:52 +0200

    bundle after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,16 +11,22 @@ set :git_enable_submodules, true
 server "arthwood@rubytime.arthwood.com", :app, :web, :db, :primary => true
 
 after "deploy:update_code", "deploy:link_configuration_files"
+after "deploy:update_code", "deploy:bundle"
 
- namespace :deploy do
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "restart-app rubytime"
-   end
-   
+namespace :deploy do
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "restart-app rubytime"
+  end
+  
   desc "Links configuration files"
   task :link_configuration_files do
     %w(database.yml).each do |i|
       run "cd #{File.join(release_path, 'config')} && ln -s #{File.join(shared_path, 'config', i)}"
     end
   end
- end
+  
+  desc "Install bundler gems"
+  task :bundle do
+    run "cd #{release_path}; bundle install"
+  end
+end
